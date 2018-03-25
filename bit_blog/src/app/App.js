9fc32c24bcb79b22cdb16eea0c9447e4505f2016
branqa <branqas@gmail.com>
2018-03-26 01:26:50 +0200

    bit-blog_almost_finish

--- bit_blog/src/app/App.js
+++ bit_blog/src/app/App.js
@@ -7,24 +7,24 @@ import AuthorsPage from './pages/authorsPage/AuthorsPage';
 import AuthorInfoPage from './pages/authorInfoPage/AuthorInfoPage';
 import Header from './partials/Header';
 import Footer from './partials/Footer';
+import NewPostPage from './pages/newPostPage/NewPostPage';
 
 
 
 class App extends Component {
   render() {
-    return (
-      <React.Fragment>
+    return <React.Fragment>
         <Header />
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/posts' component={PostPage} />
-          <Route path='/about' component={AboutPage} />
-          <Route exact path='/authors' component={AuthorsPage} />
-          <Route path='/authors/1' component={AuthorInfoPage} />
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/posts/new" component={NewPostPage} />
+          <Route path="/posts/:id" component={PostPage} />
+          <Route path="/about" component={AboutPage} />
+          <Route exact path="/authors" component={AuthorsPage} />
+          <Route path="/author:id" component={AuthorInfoPage} />
         </Switch>
         <Footer />
-      </React.Fragment>
-    );
+      </React.Fragment>;
   }
 }
 
